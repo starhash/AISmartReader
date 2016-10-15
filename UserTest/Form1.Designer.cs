68ad0b7e1d9b3e023a9568903d87605654bf410d
Harsh Srivastava <harshsri170995@outlook.com>
2016-10-15 17:44:32 +0530

    Updations, tests for user, USE:final.csv

---
+++ UserTest/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace UserTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.word = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.start = new System.Windows.Forms.ToolStripButton();
+            this.submit = new System.Windows.Forms.ToolStripButton();
+            this.ans = new System.Windows.Forms.ToolStripLabel();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.word);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(779, 596);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // word
+            // 
+            this.word.AutoSize = true;
+            this.word.Dock = System.Windows.Forms.DockStyle.Top;
+            this.word.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.word.Location = new System.Drawing.Point(3, 16);
+            this.word.Name = "word";
+            this.word.Padding = new System.Windows.Forms.Padding(16);
+            this.word.Size = new System.Drawing.Size(132, 63);
+            this.word.TabIndex = 5;
+            this.word.Text = "Word : ";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.start,
+            this.submit,
+            this.ans});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(779, 26);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // start
+            // 
+            this.start.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.start.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.start.Image = ((System.Drawing.Image)(resources.GetObject("start.Image")));
+            this.start.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(41, 23);
+            this.start.Text = "start";
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // submit
+            // 
+            this.submit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.submit.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.submit.Image = ((System.Drawing.Image)(resources.GetObject("submit.Image")));
+            this.submit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(55, 23);
+            this.submit.Text = "submit";
+            this.submit.ToolTipText = "submit";
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // ans
+            // 
+            this.ans.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(43, 23);
+            this.ans.Text = "ready";
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(3, 463);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Padding = new System.Windows.Forms.Padding(16);
+            this.radioButton4.Size = new System.Drawing.Size(773, 128);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(3, 335);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Padding = new System.Windows.Forms.Padding(16);
+            this.radioButton3.Size = new System.Drawing.Size(773, 128);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(3, 207);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Padding = new System.Windows.Forms.Padding(16);
+            this.radioButton2.Size = new System.Drawing.Size(773, 128);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radioButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(3, 79);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Padding = new System.Windows.Forms.Padding(16);
+            this.radioButton1.Size = new System.Drawing.Size(773, 128);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 596);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(0, 635);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton start;
+        private System.Windows.Forms.ToolStripButton submit;
+        private System.Windows.Forms.ToolStripLabel ans;
+        private System.Windows.Forms.Label word;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ UserTest/Form1.cs
@@ -0,0 +1,134 @@
+﻿using LAIR.Collections.Generic;
+using LAIR.ResourceAPIs.WordNet;
+using LemmaSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WordNetPlugin;
+
+namespace UserTest {
+    public partial class Form1 : Form {
+        ILemmatizer lmtz = new LemmatizerPrebuiltCompact(LemmaSharp.LanguagePrebuilt.English);
+
+        public class WordSet {
+            public string Word;
+            public string Properties;
+        }
+
+        Random r;
+        List<WordSet> _words;
+        int testIndex = -1;
+        int testCount = 20;
+
+        List<RadioButton> answers;
+        Dictionary<WordSet, bool> questions;
+        WordSet curr = null;
+        int answer = -1;
+        int sel = -1;
+
+        public Form1() {
+            InitializeComponent();
+            string[] lines = File.ReadAllLines(@"C:\Users\harsh\Documents\Visual Studio 2015\Projects\AISmartReader\AISmartReader\Pramodith\words-lim-len4_output.csv");
+            _words = new List<WordSet>();
+            foreach(string s in lines) {
+                int idx = s.IndexOf(',');
+                _words.Add(new WordSet() { Word = s.Substring(0, idx), Properties = s.Substring(idx + 1) });
+            }
+            r = new Random();
+            questions = new Dictionary<WordSet, bool>();
+            answers = new List<RadioButton>();
+            answers.Add(radioButton1);
+            answers.Add(radioButton2);
+            answers.Add(radioButton3);
+            answers.Add(radioButton4);
+        }
+
+        public Tuple<WordSet, Set<SynSet>> GetNonEmpty() {
+            WordSet w;
+            Set<SynSet> s;
+            do {
+                w = _words[r.Next(_words.Count)];
+                s = Plugin.GetSynSetsFor(lmtz.Lemmatize(w.Word.ToLower()));
+            } while (s.Count == 0);
+            return new Tuple<WordSet, Set<SynSet>>(w, s);
+        }
+
+        public void LoadQuestion() {
+            if (testIndex < testCount) {
+                List<Tuple<WordSet, Set<SynSet>>> four = new List<Tuple<WordSet, Set<SynSet>>>();
+                for (int i = 0; i < 4; i++) {
+                    four.Add(GetNonEmpty());
+                }
+                radioButton1.Text = four[0].Item2.ElementAt(0).Gloss;
+                radioButton2.Text = four[1].Item2.ElementAt(0).Gloss;
+                radioButton3.Text = four[2].Item2.ElementAt(0).Gloss;
+                radioButton4.Text = four[3].Item2.ElementAt(0).Gloss;
+                answer = r.Next(4);
+                curr = four[answer].Item1;
+                word.Text = curr.Word;
+                submit.Enabled = true;
+                start.Enabled = false;
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e) {
+            if(testIndex < testCount) {
+                LoadQuestion();
+            } else {
+                word.Text = "Test Completed.";
+                Calculate();
+            }
+        }
+
+        public void Calculate() {
+            Dictionary<string, bool> qfilter = new Dictionary<string, bool>();
+            foreach (WordSet s in questions.Keys)
+                qfilter.Add(s.Word+" = "+ s.Properties.Substring(s.Properties.LastIndexOf(',') + 1), questions[s]);
+            Dictionary<WordSet, bool> filter = questions.Where((x) => x.Value == false).ToDictionary((x) => x.Key, (y) => y.Value, questions.Comparer);
+            List<int> labels = filter.Keys.Select((x) => int.Parse(x.Properties.Substring(x.Properties.LastIndexOf(',')+1))).ToList();
+            int mode = labels.GroupBy(v => v).OrderByDescending(g => g.Count()).First().Key;
+            word.Text = "Test Complete. Level : " + mode;
+        }
+
+        private void submit_Click(object sender, EventArgs e) {
+            if (sel >= 0) {
+                RadioButton r = answers[sel];
+                bool b = sel == answer;
+                if(b) {
+                    ans.Text = "OK";
+                } else {
+                    ans.Text = "Wrong";
+                }
+                questions.Add(curr, b);
+                submit.Enabled = false;
+                start.Enabled = true;
+                start.Text = "Next";
+                testIndex++;
+                LoadQuestion();
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e) {
+            sel = 0;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e) {
+            sel = 1;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e) {
+            sel = 2;
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e) {
+            sel = 3;
+        }
+    }
+}
